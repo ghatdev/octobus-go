bd0c87967f218d26c5f703c42389d3ce5a9493e0
ghatdev <issacs98@gmail.com>
2019-01-10 11:52:22 +0900

    example updated

--- examples/logging.go
+++ examples/logging.go
@@ -7,11 +7,11 @@ import (
 )
 
 func main() {
-	logger := octobus.New(octobus.DefaultConfig("", "Default"))
+	logger := octobus.New(octobus.DefaultConfig("", "test1"))
 	err := logger.Dial()
 	if err != nil {
 		log.Println(err)
 	}
 
-	logger.Log("a", "DEBUG", "E", "p")
+	logger.Log("a", "b", "E", "p")
 }
